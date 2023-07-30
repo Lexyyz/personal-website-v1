e0079dfe1dd29be6a231b2c9314320881577e068
lexyyz <121369814+Lexyyz@users.noreply.github.com>
2023-07-30 11:42:05 +0700

    Update Work.jsx

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -11,23 +11,23 @@ const Work = () => {
           <span><i>work</i></span>
           <span><i>work</i></span>
         </div>
-        <span className="workOpening"><i>Featured Projects</i></span>
+        <span className="workOpening"><i>Featured Projects  🎟️</i></span>
       </div>
       <div className="workContainerMain">
         <div className="workMain">
           <img className="workImage" src={Picture} alt="" />
           <div className="workText">
-            <span className="workNumber">01</span>
+            <span className="workNumber"> </span>
             <div className="workTitle">
               <span></span>
-              <span><i>Tagline produk 1</i></span>
+              <span><i>Tagline produk 🛒 </i></span>
             </div>
           </div>
         </div>
         <div className="workLink">
           <a className="workLinked" href="#">
             <i class="uil uil-arrow-up-left"></i>
-            <span>Online Preview</span>
+            <span>Online Preview 🛒 </span>
           </a>
         </div>
       </div>
